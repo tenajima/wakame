6b366692b6421a57a20366f4ecb87086454fc660
Yuto Mizutani <mizutani@fringe81.com>
2019-05-07 17:09:03 +0900

    最低限インストールできるようにしました.

---
+++ mecabnome/__init__.py
@@ -0,0 +1,10 @@
+import os
+import logging
+
+
+PID = os.getpid()
+LOGGING_FORMAT = f'[{PID}] %(asctime)s %(levelname)s %(name)s :%(message)s'
+logging.basicConfig(level=logging.INFO, format=LOGGING_FORMAT)
+logging.addLevelName(logging.ERROR, 'Error')
+logging.addLevelName(logging.INFO, 'Info')
+logging.addLevelName(logging.WARNING, 'Warning')
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='mecabnome',
+    version='0.1.0',
+    author='Yuto Mizutani',
+    author_email='tenajima@gmail.com',
+    url='',
+    install_requires=[
+        'pandas',
+        'mecab-python3',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.6.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
