8987b629c9fcd64fcd5317ce700879666fe6a72d
Yuto Mizutani <i.yuto.curiosity@gmail.com>
2019-05-12 15:14:35 +0900

    formatterにblackを採用.

--- wakame/tokenizer.py
+++ wakame/tokenizer.py
@@ -1,4 +1,3 @@
-import logging
 import re
 import subprocess
 
@@ -11,21 +10,27 @@ class Token:
     """
 
     def __init__(self, word_with_feat: str):
-        surface, feature = word_with_feat.split('\t')
+        surface, feature = word_with_feat.split("\t")
 
         # mecabの仕様で,アルファベットの単語は原型が'*'になり,
         # 読みと発音が省略されるためその対応として以下のように行う.
-        split_feature = feature.split(',')
+        split_feature = feature.split(",")
         if len(split_feature) == 7:
-            part_of_speech = ','.join(split_feature[:4])
+            part_of_speech = ",".join(split_feature[:4])
             infl_type = split_feature[4]
             infl_form = split_feature[5]
             base_form = surface
-            reading = '*'
-            phonetic = '*'
+            reading = "*"
+            phonetic = "*"
         else:
-            (part_of_speech, infl_type, infl_form, base_form, reading,
-             phonetic) = feature.rsplit(',', 5)
+            (
+                part_of_speech,
+                infl_type,
+                infl_form,
+                base_form,
+                reading,
+                phonetic,
+            ) = feature.rsplit(",", 5)
 
         self.surface = surface
         """surface form (表層形)"""
@@ -43,14 +48,15 @@ class Token:
         """pronounce (発音)"""
 
     def __str__(self):
-        return '%s\t%s,%s,%s,%s,%s,%s' % \
-           (self.surface,
-           self.part_of_speech,
-           self.infl_type,
-           self.infl_form,
-           self.base_form,
-           self.reading,
-           self.phonetic)
+        return "%s\t%s,%s,%s,%s,%s,%s" % (
+            self.surface,
+            self.part_of_speech,
+            self.infl_type,
+            self.infl_form,
+            self.base_form,
+            self.reading,
+            self.phonetic,
+        )
 
 
 class Tokenizer:
@@ -60,42 +66,44 @@ class Tokenizer:
 
     def __init__(self, use_neologd=False):
         """mecabを使えるようにする
-        
+
         Parameters
         ----------
         use_neologd : bool, optional
             NEologdを使うかどうかを指定する, by default False
         """
         # get neologd
-        cmd = 'echo `mecab-config --dicdir`'
+        cmd = "echo `mecab-config --dicdir`"
         if use_neologd:
             cmd = cmd + '"/mecab-ipadic-neologd"'
         else:
             cmd = cmd + '"/ipadic"'
 
         # 辞書のパスを取得する
-        dict_path = subprocess.Popen(
-            cmd, stdout=subprocess.PIPE,
-            shell=True).stdout.readline().decode()
-        dict_path = re.sub(r'\n', '', dict_path)
+        dict_path = (
+            subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+            .stdout.readline()
+            .decode()
+        )
+        dict_path = re.sub(r"\n", "", dict_path)
 
-        self.tokenizer = MeCab.Tagger('-d ' + dict_path)
+        self.tokenizer = MeCab.Tagger("-d " + dict_path)
 
     def tokenize(self, text, wakati=False):
         """トークン化する
-        
+
         Parameters
         ----------
         text : str
             品詞分解する対象のtext
         wakati : bool, optional
             Trueにするとsurface部分のみをリストで返すようにする, by default False
-        
+
         Returns
         -------
         トークンのリスト(wakati=False),もしくは文字列のリスト(wakati=True)
         """
-        chunk: str = self.tokenizer.parse(text).split('\n')[:-2]
+        chunk: str = self.tokenizer.parse(text).split("\n")[:-2]
         if wakati:
             tokens = [Token(ch).surface for ch in chunk]
         else:
